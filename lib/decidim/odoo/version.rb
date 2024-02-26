d04287ece3a0ead514674deaed3aba3438586785
Fran Bolívar <francisco.bolivar@nazaries.com>
2024-02-26 15:11:17 +0100

    Prepare release 0.2.1 (#20)

--- lib/decidim/odoo/version.rb
+++ lib/decidim/odoo/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Odoo
-    VERSION = "0.2.0"
+    VERSION = "0.2.1"
     DECIDIM_VERSION = "0.27.3"
     COMPAT_DECIDIM_VERSION = [">= 0.27.0", "< 0.28"].freeze
   end
